cfaea59664adf8c6a26dc58c41bc0bf0527d311b
Dmitry Stupin <dm.stup@mail.ru>
2025-01-13 13:27:16 +0700

    end lesson_4

--- script.js
+++ script.js
@@ -1,122 +1,86 @@
 /*
- В JS есть 8 типов данных:
- - 6 примитивных
- - 2 специальных
-*/ 
-const name = 'Dmitry' // String
-const age = 19 // Number
-const isDeveloper = false // Boolean
-const power = 9999999999999n // BigInt
-const sign = Symbol() // Symbol
-const something = undefined // Undefined
-
-// Object
-const user = {
-  login: 'l1',
-  password: 'p1'
-}
-
-const data = null // Null
-
-
-/*
-  String:
-  В строках можно использовать '', "", ``.
-  Одинарные и двойные ничем не отличаются, а вот "бэктики" используются для интерполяции строк
-  Также через бэктики можно разбить строку на несколько строк
-*/
-const message = `
-  Привет, ${name}!
-  Тебе же ${age} лет?
- `
-console.log(message)
-
-
-/*
-  Age:
-  С числами можно производить все математические операции. Правила такие же.
-*/
-/*
-  Если попробовать поделить строку на число, то появиться специальное значение "NaN".
-  NaN - Not a Number (Не число)
-*/
-console.log('Dima' / 100)
-
-
-/*
-  Если поделить положительное/отрицательное число на 0, то мы получим
-  значение Infinity/-Infinity
-*/
-console.log(19 / 0)
-console.log(-19 / 0)
-
-
-/*
-  Boolean:
-  Есть только два значения: true и false
+  В JS есть все привычные математические операции
 */
 
 
 /*
-  Null:
-  Формально не тип, а специальное значение.
-  Null - буквально ничто, пустое значение, присваются переменным, у которых пока неизвестно 
-  значение.
+  Бинарные операторы - применяются к 2-ум операндам (числам)
+  Унарные операторы - применяются к 1-ому операнду (числу)
+*/
+console.log(-10)
+console.log(5 * -2 + 10 / -3)
+
+
+/*
+  С помощью унарного оператора "+" можно привести значение к типу Number
+*/
+console.log(+true)
+console.log(+false)
+console.log(+'123')
+console.log(+undefined)
+console.log(+null)
+
+
+/*
+  У унарных операторов приоритет выше, чем у бинарных
+*/
+console.log('3' + '5')
+console.log(+'3' + +'5')
+
+
+/*
+  С помощью +=, -+, *=, /= можно присвоить новое значение переменной
+  С конкатенацией строк всё работает также
+*/
+let a1 = 1
+a1 += 1
+console.log(a1)
+a1 *= 2
+console.log(a1)
+a1 -= 1
+console.log(a1)
+a1 /= 100
+console.log(a1)
+
+
+/*
+  Декремент - уменьшение числового значение на 1
+  Инкремент - увеличение числового значение на 1
+*/
+let count = 10
+count--
+count--
+count++
+count--
+console.log(count)
+// Мы записили их в постфиксной форме, есть также префиксная форма ++count
+// А в чем собственно разница?
+
+let num = 10
+let newNum = ++num // Мы сначала присвоили новое значение num, а затем присвоили это значение
+// и newNum и он равняется num
+console.log(num)
+console.log(newNum)
+
+let num1 = 10
+let newNum1 = num1++ // Тут мы сначала присвоили numNum1 значение num1 равное 10
+// А затем увеличили num1 на 1 и оно стало равняться 11
+console.log(num1)
+console.log(newNum1)
+
+
+/*
+  Здесь также есть операторы сравнения >, <, >=, <=, ==, !=
 */
 
 
 /*
-  undefined:
-  Буквально "необъявлено"
-*/
-let girlfriend
-console.log(girlfriend)
-
-
-/*
-  Узнать тип переменной можно через "typeof"
-*/
-const goat = 'OG Buda'
-console.log(typeof goat)
-
-
-/*
-  Явное и неявное преобразование типов.
-*/
-const num = 111
-const str = '222'
-const result = num + str
-
-console.log(result)
-console.log(typeof result)
-/*
-  У нас произошло неявное преобразование типа "number" к типу "string" 
-*/
-
-
-console.log('16' / '2')
-console.log( typeof('16' / '2'))
-/*
-  А тут уже наоборот
+  Строки сравниваются посимвольно в кодировке Unicode
 */
 
 
 /*
-  Явно можно преобразовать через String(имя переменной) и Number(имя переменной).
-  Кстати, true и false, null тоже можно привести к Number. true = 1, false,null = 0.
+  Есть строгое (без приведения типов) и нестрогое (с привидением типов) сравнение.
+  Строгое: === (Лучше пользоваться этим)
+  Нестрогое: ==
 */
-
-
-/*
-  Boolean()
-  Числа: все, кроме 0, преобразует к true, 0 = false;
-  Строки: все, кроме '', преобразует к true, '' = false
-*/
-// Преобразуются в false
-console.log(Boolean(0))
-console.log(Boolean(NaN))
-console.log(Boolean(''))
-console.log(Boolean(null))
-console.log(Boolean(undefined))
-
-// Все остальные значения - в true
