6ece745097ec9dba2292d5bf58fa261f6fe31d2d
Dmitry Stupin <dm.stup@mail.ru>
2025-01-18 15:21:49 +0700

    end lesson_11

--- script.js
+++ script.js
@@ -1,94 +1,243 @@
 /*
-  Объект предназначен для хранения непримитивных сущностей
+  Объект - ЭТО ССЫЛОЧНЫЙ ТИП ДАННЫХ.
+  JS хранит непримитивные значения по ссылке на место в памяти.
+  Соответственно => у каждого объекта выделена своя ячейка памяти (уникальная ссылка)
+   => они не могут быть одинаковыми.
 */
-const firstEmptyObject = {}
+// const obj1 = {age: 10}
+// const obj2 = {age: 10}
+
+// console.log(obj1 === obj2)
 
 
 /*
-  У объекта синтаксис ключ: значение
-  Ключ нужно оборачивать в кавычки, если оно состоит из двух слов или там есть дефис
+  Если мы запишем так - то мы НЕ ПОЛУЧАЕМ КОПИЮ, А ПРОСТО ССЫЛАЕМСЯ НА ОДНО МЕСТО В ПАМЯТИ
 */
-// const user = {
-//   login: 'Dmitry',
-//   password: 'bebe',
-//   age: 19,
-//   'study status': 'study',
-//   'last-password': 'sdfsdf',
-//   sayHi: () => console.log('Hi') // Функции внутри объектов называются МЕТОДАМИ
+// const obj3 = {name: 'Dima'}
+// const obj4 = obj3
+
+// console.log(obj3 === obj4)
+// console.log(obj3)
+
+// obj4.name = 'Lera'
+// console.log(obj3) // Хоть мы и поменяли казалось бы obj4,но при этом мы поменяли и obj3, так как у 
+// // обеиъ переменных ссылка на одну область в памяти
+
+
+/*
+  Но все-таки мы можем сделать сравнение.
+  Но оно будет поверхностным => если внутри будут еще объекты, то проверка вернет false
+*/
+// const user1 = {name: 'Dima'}
+// const user2 = {name: 'Dima'}
+
+// const areObjectsEqual = (object1, object2) => {
+//   const keys1 = Object.keys(object1)
+//   const keys2 = Object.keys(object2)
+
+//   if (keys1.length !== keys2.length) {
+//     return false
+//   }
+
+//   for (const key in object1) {
+//     if (object1[key] !== object2[key]) {
+//       return false
+//     }
+//   }
+
+//   return true
 // }
 
-
-/*
-  Чтобы получить доступ к объекту можно написать так
-*/
-// console.log(user.login)
-// console.log(user['password'])
-// user.sayHi()
+// console.log('Равны ли объекты obj1 и obj2:', areObjectsEqual(user1, user2))
 
 
 /*
-  В объект можно добавлять и удалять свойства
+  Это можно исправить:
 */
-// const admin = {}
+// const user1 = {
+//   name: 'Dima',
+//   address: {
+//     city: 'Kansk'
+//   }
+// }
+// const user2 = {
+//   name: 'Dima',
+//   address: {
+//     city: 'Kansk'
+//   }
+// }
 
-// console.log(admin)
+// const areObjectsEqual = (object1, object2) => {
+//   const keys1 = Object.keys(object1)
+//   const keys2 = Object.keys(object2)
 
-// admin.name = 'Dima'
-// admin['last-auth'] = '01.01.2025'
+//   if (keys1.length !== keys2.length) {
+//     return false
+//   }
 
-// console.log(admin)
+//   for (const key in object1) {
+//     const value1 = object1[key]
+//     const value2 = object2[key]
+//     const areValuesObjects =
+//       typeof value1 === 'object' && typeof value2 === 'object'
 
-// admin.name = 'Dimon' // Меняем значение свойства у admin
+//     if (areValuesObjects) {
+//       return areObjectsEqual(value1, value2)
+//     }
 
-// console.log(admin)
+//     if (value1 !== value2) {
+//       return false
+//     }
+//   }
 
-// delete admin['last-auth']
+//   return true
+// }
 
-// console.log(admin)
+// console.log('Равны ли объекты obj1 и obj2:', areObjectsEqual(user1, user2))
+
+
 /*
-  Мы изменяем содержимое объекта, а не переопределяем сам объект, поэтому const нам в этом не мешает
+  Мы уже копировали объекты, но это происходило через ссылку на конкретное место в памяти
+  И при изменении у какого-то из объектов значения свойства - оно менялось и у другого.
+
+  Нам нужно копировать иначе:
+*/
+
+
+// const obj5 = {name: 'Dima'}
+// const obj6 = Object.assign({}, obj5) // Мы задаем стартовый объект, а затем объект у которого берем свойства
+
+// obj6.name = 'Lera'
+
+// console.log(obj5)
+// console.log(obj6)
+
+
+/*
+  Либо можно воспользоваться Spread оператором
+*/
+// const obj7 = {name: 'Dima'}
+// const obj8 = { ...obj7 } // Указываем 3 точки (Это оператор расширения)
+
+// obj8.name = 'Lera'
+
+// console.log(obj7)
+// console.log(obj8)
+
+
+/*
+  Вот объединять объекты можно как раз через Object.assign
+*/
+// const obj9 = {name: 'Dima'}
+// const obj10 = {age: 19}
+
+// const user = Object.assign({}, obj10, obj9)
+
+// console.log('user', user)
+
+
+/*
+  Важно помнить - через Object.assign и Spread операторы только поверхностное копирование
+  Если нам захочется делать его глубоким, то либо:
+  1. Мы делаем это через костыли
+  2. Либо пользуемся библиотекой lodash
 */
 
 
 /*
-  Можно сокращенно записать свойства у объекта
+  Опициональная цепочка "?."
 */
-const name = 'Kanst'
-const homeTown = 'Kansk'
+// const user = {
+//   name: 'Dimka',
+//   age: 19,
+//   // address: {
+//   //   city: "Krasnoyarsk",
+//   //   street: 'Borisova'
+//   // }
+// }
 
-const goat = {
-  name,
-  homeTown,
+// console.log(user.address?.city) // Мы проверяем есть ли такой объект и если его не будет, то просто
+// // появиться undefined
+
+
+/*
+  Деструктуризация объекта
+*/
+// const user = {
+//   name: 'Dima',
+//   age: 19,
+//   isDeveloper: false
+// }
+
+// const { name, age, isDeveloper } = user // Вот и деструктуризация
+
+// console.log(name)
+// console.log(age)
+// console.log(isDeveloper)
+
+
+/*
+  Деструктуризация в параметрах функции
+*/
+// const logAddress = ({city, street}) => {
+
+//   console.log(`Город: ${city}, Улица: ${street}`)
+// }
+
+// logAddress({
+//   city: 'Krasnoyarsk',
+//   street: 'Borisova'
+// })
+
+/*
+  Деструктуризация с переименованием
+*/
+// const user = {
+//   name: 'Dima'
+// }
+
+// const admin = {
+//   name: 'Kirill'
+// }
+
+// const {name: userName} = user
+// const {name: adminName} = admin
+
+// console.log(userName)
+// console.log(adminName)
+
+
+/*
+  Значение по умолчанию.
+  Если вдруг значения будет, то выведется значение по умолчанию
+*/
+// const user1 = {
+//   age: 19,
+//   city: 'Moscow'
+// }
+
+// const user2 = {
+//   age: 19
+// }
+
+// const { city = 'Не указано' } = user2
+
+// console.log(city)
+
+
+/*
+  Остаточные (Или Rest-параметры)
+  Указываются ОБЯЗАТЕЛЬНО В КОНЦЕ через 3 точки.
+  Если в объекте ничего дополнительного не будет, то ничего и не выведется
+*/
+const logAddress = ({city, street, ...otherInfo}) => {
+
+  console.log(`Город: ${city}, Улица: ${street}`)
+  console.log(otherInfo)
 }
 
-// const goat = {name, homeTown} ну или записать так
-
-// console.log(goat)
-
-
-/*
-  Имена свойств(ключи) - ЭТО ВСЕГДА СТРОКИ, ДАЖЕ ЕСЛИ ТАМ ЗАПИСАТЬ ЧИСЛО.
-  Можно использовать даже те слова, которые зарезервированы JS.
-  Имена могут быть вычисляемыми
-*/
-
-
-/*
-  Проверка наличия свойства у объекта
-*/
-// console.log('name' in goat)
-// console.log('girlfriend' in goat)
-
-
-/*
-  Перебор свойств объекта через цикл for in
-*/
-for (const key in goat) { 
-  console.log(key) // Получение имен свойств
-  console.log(goat[key]) // Получение значений свойста
-}
-
-
-/*
-  Свойства объектов не всегда выводятся в том порядке, в котором мы их задаем в коде
-*/
+logAddress({
+  city: 'Krasnoyarsk',
+  street: 'Borisova',
+  country: 'Russia'
+})
