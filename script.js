690ec70e1a4f1ae50e4257b3d639a99596981eee
Dmitry Stupin <dm.stup@mail.ru>
2025-02-09 10:25:35 +0700

    end lesson_38

--- script.js
+++ script.js
@@ -1,59 +1,46 @@
-// Доступ ко всем DOM-элементам form на странице - свойство document.forms
-// console.log('Все элементы form на странице:', document.forms)
-// console.log('Форма регистрации:', document.forms[0])
+// Отмена станадартной браузерной отправки формы
+const formElement = document.querySelector('form')
+
+
+// Короче, это не самый лучший способо сбора данных
+// formElement.addEventListener('submit', (event) => {
+//   event.preventDefault()
+
+//   // Структура данных для ххранения информации о форме
+//   const formData = {
+
+//   }
+
+//   for (const element of formElement.elements) {
+//     if (!element.name) {
+//       continue
+//     }
+
+//     if (element.type === 'checkbox') {
+//       formData[element.name] = element.checked
+//       continue
+//     }
+
+//     if (element.type === 'radio' && !element.checked) {
+//       continue
+//     }
+
+//     formData[element.name] = element.value
+
+//   }
+//   console.log('formData', formData)
+// })
 
 
 
-// Доступ к DOM-элементам полей формы - свойство formElement.elements.elementName
-const formElement = document.querySelector('#regForm')
+// Вот способ лучше: Сбор данных с формы - класс FormData
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
 
-// console.log('Элементы input, textarea и select внутри формы:', formElement.elements)
+  const formData = new FormData(formElement)
 
-// console.log('Поле ввода логина:', formElement.login)
-// console.log('Поле ввода пароля:', formElement.password)
-// console.log('Поле ввода пароля:', formElement.gender) // получим список
-
-
-
-// Доступ к форме, к которой привязан DOM-элемент - свойство и атрибут form
-// const loginInput = formElement.login
-// const passwordInput = formElement.password
-
-// console.log('К какой форме относится поле ввода логина?', loginInput.form)
-// console.log('К какой форме относится поле ввода пароля?', passwordInput.form)
-// console.log('К какой форме относится поле ввода пароля?', document.querySelector('#city').form)
-/*
-  В HTML-разметке выбор города не относится к основной форме, поэтому для их связки нужно воспользоваться
-  атрибутом form="" в котором нужно указать id формы к которой нужно привязаться
-*/
-
-
-
-// Управление элементами классического input и textarea
-// const loginInputElement = formElement.login
-// const aboutInputElement = formElement.about
-
-// // Программно устанавливаем значения
-// loginInputElement.value = 'Dmitry'
-// aboutInputElement.value = 'Frontend Developer'
-
-
-
-// Управление элементами input type radio
-// const genderRadios = formElement.gender
-
-// console.log('Информация о гендере:', genderRadios.value)
-
-
-
-// Управление элементом input type checkbox
-// const agreementCheckboxElement = formElement.agreement;
-
-// console.log('Согласен ли на всё?', agreementCheckboxElement.checked)
-
-
-
-// Управление элементом select
-const citySelectElement = formElement.city;
-
-console.log('Выбранный город:', citySelectElement.value)
+  // formData.forEach((value, name) => {
+  //   console.log(`${name}: ${value}`)
+  // })
+  console.log(Object.fromEntries(formData))
+})
