5dd80c9f06bd1faa1a630e49570ee5d10bf9dc3b
Dmitry Stupin <dm.stup@mail.ru>
2025-02-10 14:03:50 +0700

    end lesson_41

--- script.js
+++ script.js
@@ -1,83 +1,107 @@
-const loadPostFormElement = document.querySelector('.load-post-form')
-const postIdInputElement = document.querySelector('#post-id')
-const resultElement = document.querySelector('.result')
+// Cookie в браузере
+// console.log(document.cookie)
 
-loadPostFormElement.addEventListener('submit', (event) => {
-  event.preventDefault()
+// document.cookie = 'username=Dmitry'
 
-  fetch(`http://localhost:3000/posts/${postIdInputElement.value}`)
-      .then((response) => {
-        console.log('response:', response)
 
-        if (!response.ok) {
-          const errorMessage = response.status === 404
-            ? 'Пост по указанному идентификатору не найден'
-            : 'Что-то пошло не так :('
 
-          throw new Error(errorMessage)
-        }
+// Браузерные хранилища SessionStorage и LocalStorage
 
-        return response.json()
-      })
-      .then((json) => {
-        console.log(json)
+/*
+  Cookie:
+  - данные сохраняются после перезагрузки страницы;
+  - данные привязаны к конкретному домену;
+  - могут управляться сервером;
+  - количество записей ограничено ~20 шт;
+  - каждая запись хранит не более 4 Кб данных;
+  - для чтения, добавления, изменения и удаления записей нужны кастомные утилитарные функции
+*/
+/*
+  SessionStorage и LocalStorage:
+  - данные сохраняются после перезагрузки страницы;
+  - данные привязаны к конкретному домену;
+  - не могут управляться сервером;
+  - количество и объем записей ограничен 5 Мб данных;
+  - для чтения, добавления, изменения и удаления записей есть встроенные методы.
 
-        const { title, views } = json
+  SessionStorage:
+  - существует в рамках текущей активной вкладки.
 
-        resultElement.innerHTML = `
-          <p>${title}, просмотров: ${views}</p>
-        `
-      })
-      .catch((error) => {
-        console.log('error:', error)
+  LocalStorage:
+  - данные не имеют срока давности.
+*/
 
-        resultElement.innerHTML = error.message
-      })
-})
 
-const createPostFormElement = document.querySelector('.create-post-form');
 
-createPostFormElement.addEventListener('submit', (event) => {
-  event.preventDefault()
+// console.log(sessionStorage)
+// console.log(localStorage)
 
-  const formData = new FormData(createPostFormElement)
-  const formDataObject = Object.fromEntries(formData)
+// sessionStorage.setItem('username', 'Dmitry')
+// sessionStorage.setItem(
+//   'user',
+//   JSON.stringify({
+//     name: 'Kanst',
+//     age: 19,
+//     isDeveloper: false,
+//   })
+// )
 
-  fetch('http://localhost:3000/posts', {
-    method: 'post',
-    body: JSON.stringify({
-      ...formDataObject,
-      views: 0
-    })
-  }).then((response) => {
-    console.log('response', response)
+// console.log('username', sessionStorage.getItem('username'))
+// console.log('user:', sessionStorage.getItem('user'))
+// sessionStorage.removeItem('username')
+// console.log('username', sessionStorage.getItem('username'))
 
-    return response.json()
-  }) .then((json) => {
-    console.log('json:', json)
-  })
-})
+// sessionStorage.clear()
 
-const searchPostFormElement = document.querySelector('.search-posts-form')
-const postViewsInputElement = document.querySelector('#post-views')
+// console.log(sessionStorage)
 
-searchPostFormElement.addEventListener('submit', (event) => {
-  event.preventDefault()
 
-  fetch(`http://localhost:3000/posts?views_gte=${postViewsInputElement.value}`)
-    .then((response) => {
-      return response.json()
-    })
-    .then((json) => {
-      console.log('json:', json)
 
-      resultElement.innerHTML = json
-        .map(({ title, views }) => {
-          return `
-            <p>${title}, просмотров: ${views}</p>
-          `
-        })
-        .join('')
-    })
-})
+class ThemeSwitcher {
+  selectors = {
+    switchThemeButton: '[data-js-theme-switcher]'
+  }
 
+  themes = {
+    dark: 'dark',
+    light: 'light',
+  }
+
+  stateClasses = {
+    isDarkTheme: 'is-dark-theme',
+  }
+
+  storageKey = 'theme'
+
+  constructor() {
+    this.switchThemeButtonElement = document.querySelector(this.selectors.switchThemeButton)
+    this.setInitialTheme()
+    this.bindEvents()
+  }
+
+  get isDarkThemeCached() {
+    return localStorage.getItem(this.storageKey) === this.themes.dark
+  }
+
+  setInitialTheme() {
+    document.documentElement.classList.toggle(
+      this.stateClasses.isDarkTheme,
+      this.isDarkThemeCached
+    )
+  }
+
+  onClick = () => {
+    localStorage.setItem(
+      this.storageKey,
+      this.isDarkThemeCached ? this.themes.light : this.themes.dark
+    )
+
+    document.documentElement.classList.toggle(this.stateClasses.isDarkTheme)
+  }
+
+  bindEvents() {
+    this.switchThemeButtonElement.addEventListener('click', this.onClick)
+  }
+}
+
+new ThemeSwitcher();
